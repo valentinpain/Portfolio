cae56638fb0d35ab4a0a8fdefb7a169ef10cdeff
valentinpain <valentin.pain@viacesi.fr>
2023-10-18 15:58:56 +0200

    canvas fix

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,7 +8,6 @@ const Tech = () => {
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((tech) => (
         <div className="w-28 h-28" key={tech.name}>
-          <BallCanvas icon={tech.icon} />
         </div>
       ))}
     </div>
