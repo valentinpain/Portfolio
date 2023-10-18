81e02c00abd010e3642b61679db05d72b3a8d1cd
valentinpain <valentin.pain@viacesi.fr>
2023-10-18 16:08:55 +0200

    mobile fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,8 +20,8 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.4 : 0.75}
-        position={isMobile ? [0, -2.5, -1.5] : [0, -3, -1.5]}
+        scale={isMobile ? 0.2 : 0.75}
+        position={isMobile ? [0, -1, -1] : [0, -3, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
